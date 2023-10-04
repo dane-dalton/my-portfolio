48bb03435f26fa6f53a5c1b3de0bcefb872affb9
Dane Dalton <danezdalton1@gmail.com>
2023-10-04 17:29:20 -0500

    Fix bug with prettier-tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 function App() {
   return (
     <>
-      <h1 className="bg-green-300 text-3xl mt-5  p-5 m-2 font-semibold">
+      <h1 className="m-6 mt-3 flex  rounded-lg  border-2 border-black bg-gray-300 p-4 text-blue-900 hover:bg-gray-600">
         Portfolio
       </h1>
-      <p className="text-white">Hello hello ehllo</p>
 
-      
+      <p className="m-1 mt-2 flex p-3 text-white">Hello hello ehllo</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
