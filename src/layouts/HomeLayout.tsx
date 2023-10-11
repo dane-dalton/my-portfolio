63d03b375c19bbe02cdd5780f17b00a51826e4a2
Dane Dalton <danezdalton1@gmail.com>
2023-10-11 13:26:01 -0500

    mix blend mode fix. remove mix blend mode from child if z-index is set to 2

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function HomeLayout() {
   return (
-    <div className="isolate h-screen bg-gradient-to-br from-purple-bg-1 via-purple-bg-2 via-45% to-orange-bg-1 mix-blend-multiply">
+    <div className="relative isolate h-screen w-screen before:absolute before:left-0 before:top-0 before:z-10 before:h-screen before:w-screen before:bg-gradient-to-br before:from-purple-bg-1 before:via-purple-bg-2 before:via-45% before:to-orange-bg-1 before:mix-blend-multiply before:content-[''] ">
       <div className="flex h-full w-full items-center justify-center bg-slate-900">
         <div className="absolute inset-8 flex items-center justify-center rounded-3xl bg-white font-bold">
           <div className="absolute inset-4 overflow-hidden rounded-3xl bg-slate-900">
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,10 +7,21 @@ import {
 function HomePage() {
   return (
     <>
-      <h1 className="ml-10 mt-8 text-9xl text-white underline">Portfolio</h1>
+      <h1 className="mt-8 text-center text-9xl text-white">Welcome!</h1>
       <LeftScrollArrow />
       <RightScrollArrow />
       <ImageBottomRight />
+      <section className="mt-10 flex flex-col items-center justify-center gap-10 text-xl">
+        <p className="max-w-[500px] text-white">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
+          doloremque. Reiciendis vel nobis dolorum optio dolore numquam,
+          repellat error aut quo similique quia doloremque voluptas id
+          repudiandae modi consequatur blanditiis!
+        </p>
+        <p className="text-white">
+          Scroll to learn more about me and my projects.
+        </p>
+      </section>
     </>
   );
 }
