8aa480a3461884ec81e8ab584e69c3a5ab8d6e47
dane-dalton <danedalton@sbcglobal.net>
2024-01-18 15:23:30 -0600

    ImageBottomRight variable sizing

--- src/components/ImageBottomRight.tsx
+++ src/components/ImageBottomRight.tsx
@@ -2,7 +2,7 @@ import { photoOfMe } from "../assets";
 
 function ImageBottomRight() {
   return (
-    <div className="absolute -bottom-4 -right-4 overflow-hidden rounded-full bg-white xs:h-[18rem] xs:w-[18rem] md:h-[21rem] md:w-[21rem] lg:h-[24rem] lg:w-[24rem]">
+    <div className="absolute -bottom-4 -right-4 overflow-hidden rounded-full bg-white xs:h-[16rem] xs:w-[16rem] md:h-[20rem] md:w-[20rem] lg:h-[22rem] lg:w-[22rem] xl:h-[24rem] xl:w-[24rem]">
       <img src={photoOfMe} alt="Photo of me" className="absolute z-10" />
     </div>
   );
