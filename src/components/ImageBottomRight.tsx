085448fee8830955f8e0f25c31b0b34566d6c454
dane-dalton <danedalton@sbcglobal.net>
2024-01-18 15:17:12 -0600

    HomePage styled for differing screen sizes

--- src/components/ImageBottomRight.tsx
+++ src/components/ImageBottomRight.tsx
@@ -2,7 +2,7 @@ import { photoOfMe } from "../assets";
 
 function ImageBottomRight() {
   return (
-    <div className="absolute -bottom-4 -right-4 h-[384px] w-[384px] overflow-hidden rounded-full bg-white">
+    <div className="absolute -bottom-4 -right-4 overflow-hidden rounded-full bg-white xs:h-[18rem] xs:w-[18rem] md:h-[21rem] md:w-[21rem] lg:h-[24rem] lg:w-[24rem]">
       <img src={photoOfMe} alt="Photo of me" className="absolute z-10" />
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,11 +5,11 @@ function HomePage() {
   return (
     <div className="absolute z-10 flex h-full w-full flex-col items-center">
       <RightScrollArrow pagesIdx={pageIdx} />
-      <h1 className="stroke-normal mt-[3rem] text-center text-3xl">
+      <h1 className="stroke-normal mt-[3rem] text-center xs:text-xl lg:text-3xl">
         Hello, my name is
       </h1>
       <PageHeader header="Dane Dalton" />
-      <section className="stroke-home mx-[24rem] text-center text-[3rem]">
+      <section className="stroke-home text-center xs:mx-[14rem] xs:text-[1.5rem] md:mx-[20rem] md:text-[2rem] xl:mx-[24rem] xl:text-[3rem]">
         <p>
           This website is a portfolio for some of my projects, a showcase of my
           creative expression, and an opportunity to learn TypeScript.
