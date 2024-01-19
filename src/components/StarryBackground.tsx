0e93fae1ab919a9da3169ae0399c82b52c2c868a
dane-dalton <danedalton@sbcglobal.net>
2024-01-19 14:29:42 -0600

    Star navigation started on Home Page

--- src/components/StarryBackground.tsx
+++ src/components/StarryBackground.tsx
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom";
+import usePageIdx from "../hooks/usePageIdx";
+
 function StarryBackground() {
+  const { pageIdx } = usePageIdx();
+
+  const onHomePage = () => {
+    return pageIdx === 0;
+  };
+  const onAboutPage = () => {
+    return pageIdx === 1;
+  };
+  const onProjectsPage = () => {
+    return pageIdx === 2;
+  };
+
   return (
     <div className="absolute z-10 h-full w-full">
       <div className="sky-shadow-violet-2 absolute left-[12rem] top-[15rem]" />
@@ -17,6 +32,19 @@ function StarryBackground() {
       <div className="star absolute right-[9rem] top-[4rem]" />
       <div className="star absolute right-[5rem] top-[3rem]" />
       <div className="star absolute right-[6rem] top-[9rem]" />
+
+      {onHomePage() && (
+        <>
+          <div className="star-nav absolute right-[5.5rem] top-[8.5rem] h-[12px] w-[12px] animate-pulse rounded-full bg-slate-400" />
+          <Link to={"/about"}>
+            <p className="stroke-nav absolute right-[70px] top-[9.5rem] z-10 animate-pulse text-xl">
+              About
+            </p>
+          </Link>
+        </>
+      )}
+      {onAboutPage() && <></>}
+      {onProjectsPage() && <></>}
     </div>
   );
 }
