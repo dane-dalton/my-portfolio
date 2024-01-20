c33943d023f476f52f4a08a224d3a58b128a7863
Dane Dalton <danezdalton1@gmail.com>
2024-01-19 18:12:37 -0600

    Projects scales better for smaller screens. Not ready for mobile.

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -23,11 +23,11 @@ function Project({ project }: ProjectProps) {
       {/* <h2 className="stroke-normal text-center text-3xl">{title}</h2> */}
       <div
         onClick={() => setProjectOpen(true)}
-        className="project-border-shadow mt-4 h-[10rem] w-[10rem] cursor-pointer rounded-lg border-2 border-white bg-black transition-all duration-150 ease-linear hover:h-[15rem] hover:w-[15rem]"
+        className="project-border-shadow xxs:h-[6rem] xxs:w-[6rem] mt-4 cursor-pointer rounded-lg border-2 border-white bg-black transition-all duration-150 ease-linear lg:h-[8rem] lg:w-[8rem] lg:hover:h-[13rem] lg:hover:w-[13rem] xl:h-[10rem] xl:w-[10rem] xl:hover:h-[15rem] xl:hover:w-[15rem]"
       >
         <img
           src={thumbImg}
-          className="h-[10rem] w-[10rem] rounded-lg object-center p-1 pb-2 transition-all duration-150 ease-linear hover:h-[15rem] hover:w-[15rem]"
+          className="xxs:h-[6rem] xxs:w-[6rem] rounded-lg object-center p-1 pb-2 transition-all duration-150 ease-linear lg:h-[8rem] lg:w-[8rem] lg:hover:h-[13rem] lg:hover:w-[13rem] xl:h-[10rem] xl:w-[10rem] xl:hover:h-[15rem] xl:hover:w-[15rem]"
         />
       </div>
       {projectOpen ? (
