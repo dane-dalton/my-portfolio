75cb306c8abf58225e52e6bfd38c532098a9c1b9
daned <danezdalton1@gmail.com>
2024-07-25 14:17:15 -0500

    Mising page redirects home

--- src/pages/Missing.tsx
+++ src/pages/Missing.tsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Missing() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
   return (
     <div className="flex h-full flex-col items-center justify-center text-white xs:text-3xl md:text-8xl">
-      <h1 className="mb-6">404 Page Not Found...</h1>
+      <h1 className="mb-6">Redirecting...</h1>
       <Link to="/" className="z-10 text-purple-400 underline">
         Home Page
       </Link>
