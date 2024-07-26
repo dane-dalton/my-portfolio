015f39d628b304d27b55a247fdc331baecbdbc31
daned <danezdalton1@gmail.com>
2024-07-26 00:18:14 -0500

    AboutPage grammar fix.

--- src/constants/pageText.ts
+++ src/constants/pageText.ts
@@ -4,7 +4,7 @@ const homeTextMobile =
   "Please check it out on desktop too for a superior viewing experience!";
 
 const aboutText1 =
-  "I am proud to say I've built a strong foundation in technology that helps me excel when I am handed a new problem or concept. Over the years I've built up the confidence to take on any problem I am faced head on. Nothing makes me more excited than solving an issue I previously thought was above me.";
+  "I am proud to say I've built a strong foundation in technology that helps me excel when I am handed a new problem or concept. Over the years I've built up the confidence to take on any problem I am faced with head on. Nothing makes me more excited than solving an issue I previously thought was above me.";
 
 const myEmail = "danezdalton1@gmail.com";
 const myLocation = "Huntsville, AL";
